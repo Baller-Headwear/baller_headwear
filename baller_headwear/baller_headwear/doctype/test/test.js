69b2a8136ee9399554e0304ce256b58727cfd28e
frappeuser <frappeuser@ballerheadweardev.us-central1-a.c.erpnext-428504.internal>
2024-12-14 11:08:25 +0000

    Update

--- baller_headwear/baller_headwear/doctype/test/test.js
+++
@@ -1,8 +0,0 @@
-// Copyright (c) 2024, manan@unifyxperts.com and contributors
-// For license information, please see license.txt
-
-frappe.ui.form.on('Test', {
-	// refresh: function(frm) {
-
-	// }
-});
